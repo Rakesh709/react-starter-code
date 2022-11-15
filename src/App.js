2c0832065c047c7aa14823cd668b203775460873
Rakesh709 <kumar.rakesh1997@outlook.com>
2022-11-15 23:26:25 +0530

    Initial React Starter Code

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Footer from './Components/Footer/Footer'
+import Navbar from './Components/Navbar/Navbar'
+
+function App() {
+  return (
+    <div>
+      <Navbar/>
+      <Footer/>
+    </div>
+  )
+}
+
+export default App
